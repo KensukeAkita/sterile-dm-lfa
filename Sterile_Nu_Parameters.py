723be9eacaf9b100f936540cd27346a2c90a229d
Kensuke Akita <68922318+KensukeAkita@users.noreply.github.com>
2025-04-17 17:31:32 +0900

    Add files via upload

---
+++ Sterile_Nu_Parameters.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Constants import * 
+
+#Lepton asymmetries
+Le_ini = 0.15
+Lmu_ini = -0.15
+Ltau_ini = 0
+
+#Sterile neutrino parameters
+
+Uas = (2*10**(-14))**(1/2)
+ms = 10*1e-3 #keV->MeV 
+flavor = 'e' #'e' or 'mu' or 'tau' The type of sterile neutrino mixing
+
+
+#QCD cutoff scale
+
+LambdaQCD = 150 #MeV
+Tcut = 250 #MeV
+
+
+
+
