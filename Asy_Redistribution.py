68089a85632e6d95d5440e01a6f7165e339a8210
Kensuke Akita <68922318+KensukeAkita@users.noreply.github.com>
2025-07-24 15:33:03 +0900

    Update Asy_Redistribution.py

--- Asy_Redistribution.py
+++ Asy_Redistribution.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import jit
 from Constants import *
 from QCD_GlobalParameters import * 
-from Setup_Grids import *
+from Setup_Bins import *
 from Thermodynamics.DOF import gs_below120MeV, gs_above120MeV
 
 ChiData = np.loadtxt("ChiTable.dat")
